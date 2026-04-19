5bbc7f59c5b93063ffc55b6d5dfda7a0eb889d26
Ayomide <144212574+mideyy7@users.noreply.github.com>
2026-04-19 21:19:58 +0100

    Add: palindrome-linked-list - submission-0

---
+++ Data Structures & Algorithms/palindrome-linked-list/submission-0.py
@@ -0,0 +1,47 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        # find mid point
+        # reverse second half
+        # check if they are equal
+
+        if not head:
+            return True
+
+        slow = head
+        fast = head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        prev = None
+        while slow:
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
+        
+        first = head
+        second = prev
+        while first:
+            if first.val != second.val:
+                return False
+            first = first.next
+            second = second.next
+        return True
+   
+      
+
+        
+        """
+
+        1 -> 2 -> 3 -> 4 -> Null
+
+        1 -> 2 -> 3 -> Null
+
+        """
+        
