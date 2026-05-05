202dc9c56e87c87e6539a8d20abe826ad72a0bf8
Ayomide <144212574+mideyy7@users.noreply.github.com>
2026-05-05 10:55:29 +0100

    Add: can-place-flowers - submission-1

---
+++ Data Structures & Algorithms/can-place-flowers/submission-1.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+       // nothing before and nothing at the front
+       // keep track if i place previously
+       // [ A, B, C, D ]
+       // if !prev_seen and (i == flower.size() - 1 or flowerbed[i + 1] == 0)_: can place
+
+       /*
+            flowerbed=[1,0,0,0,1]
+            n=1
+
+            flowerbed=[0,0,1,0,0,0,0,0,1,0,0]
+                    n=4
+       */
+        bool prev_seen = false;
+        int count = 0;
+        int size = flowerbed.size();
+        for (int i = 0; i < size; ++i) {
+            if (flowerbed[i] == 1) {
+                prev_seen = true;
+                continue;
+            }
+            if (!prev_seen && (i == size - 1 || flowerbed[i + 1] == 0)) {
+                prev_seen = true;
+                count += 1;
+            } else {
+                prev_seen = false;
+            }
+        }
+        if (count >= n) {
+            return true;
+        }
+        return false;
+    }
+};
