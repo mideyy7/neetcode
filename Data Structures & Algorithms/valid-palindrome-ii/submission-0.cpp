465c10378f2f8223491e3461dc78b66644d569cf
Ayomide <144212574+mideyy7@users.noreply.github.com>
2026-05-06 18:24:29 +0100

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int l = 0;
+        int r = s.size();
+        while (l < r) {
+            if (s[l] != s[r]) {
+                return is_palindrome(s, l + 1, r) || is_palindrome(s, l , r - 1);
+            }
+            l += 1;
+            r -= 1;
+        }
+        return true;
+        
+    }
+
+    bool is_palindrome(const string& s, int l, int r) {
+        while (l < r) {
+            if (s[l] != s[r]) {
+                return false;
+            }
+            l += 1;
+            r -= 1;
+        }
+        return true;
+    }
+};
