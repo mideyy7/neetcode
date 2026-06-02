8a0b8ee9e09fa76db17afd41ac74575c7955f1ca
Ayomide <144212574+mideyy7@users.noreply.github.com>
2026-06-02 17:24:15 +0100

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        vector<int> slice1(nums.begin() + 1, nums.end());   
+        vector<int> slice2(nums.begin(), nums.end() - 1); 
+        vector<int> dp1(slice1.size(), -1);
+        vector<int> dp1(slice2.size(), -1);
+        int result = max(solve(0, slice1, dp1), solve(0, slice2, dp2));
+        return result;
+
+
+    }
+    int solve(int n, vector<int>& nums, vector<int>& dp) {
+        if(n >= nums.size()) {
+            return 0;
+        }
+        if (dp[n] != -1) return dp[n];
+        return dp[n] = max(nums[n] + solve(n + 2, nums, dp), solve(n + 1, nums, dp));
+    }
+};
