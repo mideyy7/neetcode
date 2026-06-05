460cc5bc93185e862a87e3b9fa1568ec33ca2f25
Ayomide <144212574+mideyy7@users.noreply.github.com>
2026-06-05 01:46:31 +0100

    Add: perfect-squares - submission-1

---
+++ Data Structures & Algorithms/perfect-squares/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> dp(n + 1, -1);
+        return solve(n, dp);
+    }
+    int solve(int n, vector<int>& dp) {
+        if (n == 0) return 0;
+        if (dp[n] != -1) return dp[n];
+        int result = INT_MAX;
+        for (int s = 1; s * s <= n; ++s) {
+            int value = 1 + solve(n - (s * s), dp);
+            result = min(result, value);
+        }
+        return dp[n] = result;
+    }
+};
