ba00ffbdfad9e62a277d292ff251ae347be2cd22
Ayomide <144212574+mideyy7@users.noreply.github.com>
2026-06-07 22:09:06 +0100

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+
+        /*
+        base case is if n == 0: return 1
+        if n < 0: return 0 as this is not a valid path
+        solve(n - 1) + solve(n - 2)
+        */
+
+        vector<int>dp(n + 1, -1);
+        return solve(n, dp);
+        
+    }
+
+    int solve (int n, vector<int>& dp) {
+        if (n == 0) return 1;
+        if (n < 0) return 0;
+        if (dp[n] != -1) return dp[n];
+        return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+    }
+};
