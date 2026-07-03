eb2df3df9ce78714eb85856a2788ff0503726e41
Ayomide <144212574+mideyy7@users.noreply.github.com>
2026-07-03 18:30:13 +0100

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        std::map<char,char> mp = {{')', '('}, {'}', '{'}, {']','['}};
+        std::stack<char> st;
+
+        for (char c : s) {
+            if (mp.find(c) != mp.end()){
+                if (st.empty()) return false;
+                char top = st.top(); st.pop();
+                if (top != mp[c]) return false;
+            } else {
+                st.push(c);
+            }
+        }
+        return (st.empty()) ? true : false;
+        
+    }
+};
